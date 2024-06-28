cdf82871eb045cdd1193910ee4d6c064511c7aee
SebitasCo <sebastianmg1700@gmail.com>
2024-06-28 08:46:46 -0500

    finish

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,57 @@ const prompt = promptSync();
 
 const lista1 = new listas('lista 1', 'Sección libros nuevos', libros);
 
+function MostrarLibros(): void {
+    const libros = lista1.ObtenerLibros();
+    if (libros.length === 0) {
+        console.log('No hay libros en la sección.');
+    } else {
+        libros.forEach(librx => {
+            console.log(`
+                Título: ${librx.Titulo},
+                Autor: ${librx.Autor},
+                Género: ${librx.Genero},
+                Idioma: ${librx.Idioma},
+                Precio: ${librx.Precio},
+                Formato: ${librx.Formato},
+                ISBN: ${librx.ISBN},
+                Descripción: ${librx.Descripcion},
+                Estado: ${librx.Estado},
+                Ubicación: ${librx.Ubicacion},
+                Fecha de Publicación: ${librx.FechaPublicacion.toLocaleDateString()},
+                Editorial: ${librx.Editorial},
+                Número de Páginas: ${librx.Paginas},
+                Peso: ${librx.Peso} kg`);
+        });
+    }
+}
 
+function eliminarLibro(): void {
+    const Titulo = prompt('Ingrese el titulo del libro a eliminar: ');
+    lista1.EliminarLibro(Titulo);
+    console.log('Libro eliminado correctamente.');
+}
+
+function agregarLibro(): void {
+    const Titulo = prompt("Ingrese el título del libro: ");
+    const Autor = prompt("Ingrese el autor del libro: ");
+    const Genero = prompt("Ingrese el género del libro: ");
+    const Idioma = prompt("Ingrese el idioma del libro: ");
+    const Precio = parseFloat(prompt("Ingrese el precio del libro: "));
+    const Formato = prompt("Ingrese el formato del libro: ");
+    const ISBN = prompt("Ingrese el ISBN del libro: ");
+    const Descripcion = prompt("Ingrese la descripción del libro: ");
+    const Estado = prompt("Ingrese el estado del libro (nuevo/uso): ");
+    const Ubicacion = prompt("Ingrese la ubicación del libro: ");
+    const FechaPublicacion = new Date(prompt("Ingrese la fecha de publicación del libro: "));
+    const Editorial = prompt("Ingrese la editorial del libro: ");
+    const Paginas = parseInt(prompt("Ingrese el número de páginas del libro: "), 10);
+    const Peso = parseInt(prompt("Ingrese el peso del libro: "), 10);
+
+    const librx = new libro( Titulo, Autor,Genero,Idioma,Precio,Formato,ISBN,Descripcion,Estado,Ubicacion,FechaPublicacion,Editorial,Paginas,Peso);
+    lista1.AgregarLibro(librx);
+    console.log('Libro agregado correctamente.');
+}
 
 function menu(): void {
     let opcion: string;
@@ -23,59 +73,13 @@ function menu(): void {
 
         switch (opcion) {
             case '1':
-                function MostrarLibros(): void {
-                    const libros = lista1.ObtenerLibros();
-                    if (libros.length === 0) {
-                        console.log('No hay libros en la sección.');
-                    } else {
-                        libros.forEach(librx => {
-                            console.log(`
-                                Título: ${librx.Titulo},
-                                Autor: ${librx.Autor},
-                                Género: ${librx.Genero},
-                                Idioma: ${librx.Idioma},
-                                Precio: ${librx.Precio},
-                                Formato: ${librx.Formato},
-                                ISBN: ${librx.ISBN},
-                                Descripción: ${librx.Descripcion},
-                                Estado: ${librx.Estado},
-                                Ubicación: ${librx.Ubicacion},
-                                Fecha de Publicación: ${librx.FechaPublicacion.toLocaleDateString()},
-                                Editorial: ${librx.Editorial},
-                                Número de Páginas: ${librx.Paginas},
-                                Peso: ${librx.Peso} kg`);
-                        });
-                    }
-                }
-                break;
+               MostrarLibros();
+                break
             case '2':
-                function eliminarLibro(): void {
-                    const Titulo = prompt('Ingrese el titulo del libro a eliminar: ');
-                    lista1.EliminarLibro(Titulo);
-                    console.log('Libro eliminado correctamente.');
-                }
+               eliminarLibro()
                 break;
             case '3':
-                function agregarLibro(): void {
-                    const Titulo = prompt("Ingrese el título del libro: ");
-                    const Autor = prompt("Ingrese el autor del libro: ");
-                    const Genero = prompt("Ingrese el género del libro: ");
-                    const Idioma = prompt("Ingrese el idioma del libro: ");
-                    const Precio = parseFloat(prompt("Ingrese el precio del libro: "));
-                    const Formato = prompt("Ingrese el formato del libro: ");
-                    const ISBN = prompt("Ingrese el ISBN del libro: ");
-                    const Descripcion = prompt("Ingrese la descripción del libro: ");
-                    const Estado = prompt("Ingrese el estado del libro (nuevo/uso): ");
-                    const Ubicacion = prompt("Ingrese la ubicación del libro: ");
-                    const FechaPublicacion = new Date(prompt("Ingrese la fecha de publicación del libro: "));
-                    const Editorial = prompt("Ingrese la editorial del libro: ");
-                    const Paginas = parseInt(prompt("Ingrese el número de páginas del libro: "), 10);
-                    const Peso = parseInt(prompt("Ingrese el peso del libro: "), 10);
-                
-                    const librx = new libro( Titulo, Autor,Genero,Idioma,Precio,Formato,ISBN,Descripcion,Estado,Ubicacion,FechaPublicacion,Editorial,Paginas,Peso);
-                    lista1.AgregarLibro(librx);
-                    console.log('Libro agregado correctamente.');
-                }
+                agregarLibro()
                 
                 break;
             case '4':
